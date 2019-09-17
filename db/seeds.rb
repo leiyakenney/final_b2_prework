9db19d933f8561aaf3eeaa3e1c31734cdd2f6148
Leiya Kenney <leiya.kenney@gmail.com>
2019-09-17 09:33:53 -0600

    Adds seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+leiya = Student.create(name: "Leiya Kenney")
+caro = Student.create(name: "Caroline Song")
+rox = Student.create(name: "Roxane Clement")
+mrkt = Course.create(name: "Marketing")
+calc = Course.create(name: "Calculus")
+listen = Course.create(name: "Art of Listening")
+sc_mrkt_lk = StudentCourse.create(student: leiya, course: mrkt, grade: 0.86)
+sc_mrkt_cs = StudentCourse.create(student: caro, course: mrkt, grade: 0.882)
+sc_mrkt_rc = StudentCourse.create(student: rox, course: mrkt, grade: 0.947)
