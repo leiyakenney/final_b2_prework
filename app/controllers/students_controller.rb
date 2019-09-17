79caf7f06c87dba615b16d6b8c0d22254dfb8cc4
Leiya Kenney <leiya.kenney@gmail.com>
2019-09-17 10:22:23 -0600

    Destroys studentcourse and redirects to student path

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,4 +4,12 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
+  def destroy
+    student = Student.find(params[:id])
+    course = Course.find(params[:course_id])
+    studentcourse = StudentCourse.find_by(student: student, course: course)
+    studentcourse.delete
+    redirect_to student_path(student)
+  end
+
 end
