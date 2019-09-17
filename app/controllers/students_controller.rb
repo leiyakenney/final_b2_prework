39452b1fe54dceeacd8d6c6cdf757b9ede1cca14
Leiya Kenney <leiya.kenney@gmail.com>
2019-09-17 10:49:25 -0600

    Adds create method to student controller

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,4 +12,17 @@ class StudentsController < ApplicationController
     redirect_to student_path(student)
   end
 
+  def create
+    course = Course.find(params[:course_id])
+    student = Student.create(student_params)
+    student.save
+    @student_course = StudentCourse.create!(student: student, course: course, grade: 0)
+    redirect_to student_path(student)
+  end
+
+  private
+  def student_params
+    params.permit(:name)
+  end
+
 end
